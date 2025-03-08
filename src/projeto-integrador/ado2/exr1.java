3ed31b5500c4043221ceaf8ee7e08c491441e97a
Lucas <lucasvssoares@gmail.com>
2025-03-08 00:21:40 -0300

    fix: ado2 exr1 pi

--- src/projeto-integrador/ado2/exr1.java
+++ src/projeto-integrador/ado2/exr1.java
@@ -1,26 +1,16 @@
-//1. Dados 3 valores reais (float ou double) imprimir o valor do
-// produto destes valores;
+/******************************************************************************
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package ado2.pi;
+Dados 3 valores reais (float ou double) imprimir o valor do
+produto destes valores;
 
+*******************************************************************************/
 import java.util.Scanner;
 
-/**
- *
- * @author lucas.svsantos1
- */
-public class exr1 {
-
-    /**
-     * @param args the command line arguments
-     */
+public class exr1 
+{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // TODO code application logic here
+        
         System.out.println("Digite o primeiro valor: ");
         double valor1 = scanner.nextDouble();
         
@@ -34,5 +24,4 @@ public class exr1 {
         
         System.out.println("O valor do produto é: " + produto);
     }
-    
 }
